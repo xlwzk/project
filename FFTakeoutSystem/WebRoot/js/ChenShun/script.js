5655a514b01ff7ab2ee38c647262d718e3374575
961132897 <961132897@qq.com>
2016-08-11 17:48:01 +0800

    :2016年8月11日17:47:55 陈顺提交

--- FFTakeoutSystem/WebRoot/js/ChenShun/script.js
+++ FFTakeoutSystem/WebRoot/js/ChenShun/script.js
@@ -14,6 +14,8 @@
 //	
 //	
 //}
+
+
 /**
  * 我的菜单
  * 
@@ -52,15 +54,45 @@ function ts_menu(){
 	$("#searchDiv").panel("expand");// 默认展开
 	$("#myTable").datagrid({
 		toolbar:[
-		         {text:"新增",iconCls:"icon-add",handler:function(){
-		        	if(fiag){
-		        		 $("#myTable").datagrid("appendRow",{});// 添加一个新行
-			        	 index =  $("#myTable").datagrid("getRows").length-1; // 得到新增行的下标
-			        	   $("#myTable").datagrid("beginEdit",index);// 让这一行处于编辑状态
-			        	        	    	 fiag=false;
-			        	        	    	 op="add";
-		        	}
+		         {text:"新增",iconCls:"icon-add",handler:function(){ 
 		        	 
+		        	 //添加首先添加窗口值改变
+				    	$("#mydiv").dialog({
+				    		iconCls:'icon-add',
+				    		title:"添加"	
+				    	});
+				    	$("#mydiv").dialog("open");//打开添加窗口
+				    	$(document).ready(function(){  $("input[name=muname]").focus();});
+				    	$("#addBtn").click(function(){
+				    		 
+				            $.post("cs!AddMenu.action?v="+Math.random(),$("#myfrm").serialize(),function(data){
+				            	  var o = eval("("+data+")");
+				                  if(o.addfiag){   //判断是否是否有null值
+				                
+				    		    	  $.messager.alert("添加提示","添加成功","info")
+				    		    	  $("#mydiv").dialog("close");//关闭
+				    	    				$("#myfrm").form("clear");  //表单清空
+				    	    				$("#myTable").datagrid("reload");  //从当前页加载
+				                 }else{
+				                	  $.messager.alert("添加提示","请填写完整信息","info")
+				                	 
+				                 }
+				            	
+				                  $('#mydiv').dialog({ 
+				                	  onClose: function () { 
+				                	  //解决弹出窗口关闭后，验证消息还显示在最上面 
+				                	  $('.tooltip.tooltip-right').hide(); 
+				                	  } 
+				                	  });
+				            })
+				      
+				    		
+				    	})
+				    	 $("#quxiao").click(function(){         //单机取消时
+	    		         $("#mydiv").dialog("close");//关闭窗口
+	    	})
+				    	
+		        	
 		        	
 		         }},
 		         {text:"删除",iconCls:"icon-remove",handler:function(){
@@ -92,95 +124,77 @@ function ts_menu(){
 		        	 
 		         }},
 		         {text:"修改",iconCls:"icon-edit",handler:function(){
-		        	 var rows =  $("#myTable").datagrid("getSelections");
-		        	 if(rows.length==0){
-		        		 $.messager.alert("提示","请选择一行","info")
-		        	 }else if(rows.length>1){
-		        		  $.messager.alert("提示","一次只能选择一行","info")
-		        	 }else{
-		        		   if(fiag){
-		        			   op="update";  // 是修改操作
-		        			   fiag=false; // 表示现在不能做其他操作
-		        			   r=rows[0]; // 获得选中的行
-		        			   // 根据行来取到当前的下标
-		        			   index=$("#myTable").datagrid("getRowIndex",r);  // 通过行来取到当前的下标
-		        		       $("#myTable").datagrid("beginEdit",index); // 让此行处于编辑状态
-		        		   }
-		        	 }
+//		        
+		        		var rows=$("#myTable").datagrid("getSelections");
+						if(rows.length==0){
+							   $.messager.alert("修改提示",'请选择勾上修改内容','warning'); 
+							
+						}else if(rows.length>1){		
+							  $.messager.alert("修改提示",'抱歉！一次只能修改一行','warning'); 
+						}else{
+							$("#mydiv").dialog({
+								conCls:"icon-update",
+							    title:"修改"
+							})
+						  
+							var r=rows[0]//通过下标取到选中的行
+							           
+					    $("#myfrm").form("load",{"muname":r.MUNAME,"muprice":r.MUPRICE,"mutype":r.MUTYPE,"mudesc":r.DESC,"mustatus":r.MUSTATUS,"mudesc":r.MUDESC});
+							   $("img[name=image]").attr("src",r.MUPIC);
+							  
+							$("#mydiv").dialog("open");
+							
+							$("#addBtn").click(function(){   //执行修改操作
+							    $.post("cs!UpdateMenu.action?v="+Math.random()+"&muid="+r.MUID,$("#myfrm").serialize(),function(data){
+							    $("#mydiv").dialog("close");//关闭对话框
+							    $("#myfrm")[0].reset(); //重置表单数据 
+							    $("#myTable").datagrid("reload");//重新加载数据	
+							    var fiag=eval("("+data+")");
+							    if(fiag.updatefiag){
+							         $.messager.alert("修改提示","修改成功","info");
+					
+							         checkOnSelect =true;
+						             $("#myfrm").datagrid("selectRow", rowIndex);
+							    }else{
+							    	  $.messager.alert("修改提示","修改成失败","info");
+							    }
+							    })
+							    
+								
+							})
+					    	 $("#quxiao").click(function(){         //单机取消时
+			    		         $("#mydiv").dialog("close");//关闭窗口
+			    	})
+						
+						
+							
+						}
+		        	 
+		        	 
+		        	 
+		        	 
+		        	 
+		        	 
 		        	 
 		        	 
 		         }},
-		         {text:"保存",iconCls:"icon-save",handler:function(){
-		        	 $("#myTable").datagrid("endEdit",index); // 结束编辑状态
-		        	 fiag=true;
-		        	
-		         }},
-		         {text:"取消",iconCls:"icon-cancel",handler:function(){
-		        	 $("#myTable").datagrid("rejectChanges");// 回滚操作，让没有保存的数据，全部取消
-		        	 flag = true;
+		       
+		         {text:"导出Excl数据",iconCls:"icon-undo",handler:function(){
 		        	 
-		         }}
+		        	// $.post("cs!ExportMenu.action?v="+Math.random(),null,function(){})
+		        	 window.location="cs!ExportMenu.action?v="+Math.random();
+		         }},
+		       
+		         {text:"查看数据图表",iconCls:"icon-redo",handler:function(){
+		        	 window.location="pages/chenshun/dataview.jsp"  
+		     
+		         }},
+		       
 			
 		],
-		// 当结束编辑的时候的必须运行的事件
-		onAfterEdit:function(index,r){// 当前下标，当前行的数据（JSon）
-		
-		     if(op=="add"){
-		    $.post("cs!AddMenu.action?v="+Math.random(),r,function(data){
-		          	var fiag=eval("("+data+")");
-		          	if(fiag.addfiag>0){
-		          		$.messager.show({
-			          		showType:"slide",
-			          		showSpeed:600,
-			          		title:"提示",
-			          		msg:"添加成功",
-			          		timeout:3000   		
-			          	});        		
-		          	}else{
-		          		$.messager.show({
-			          		showType:"slide",
-			          		showSpeed:600,
-			          		title:"提示",
-			          		msg:"添加失败",
-			          		timeout:3000   		
-			          	});      
-		          	}
-		             
-		    }) 
-		    $("#mytable").datagrid("load");
-		     }
-		     if(op=="update"){
-		    	 $.post("cs!UpdateMenu.action?v="+Math.random(),r,function(data){
-		    		 var fiag=eval("("+data+")");
-		             if(fiag.updatefiag>0){
-		            	 $.messager.show({
-				          		showType:"slide",
-				          		showSpeed:600,
-				          		title:"提示",
-				          		msg:"修改成功",
-				          		timeout:3000   		
-				          	}); 
-		             }else{
-		            	 $.messager.show({
-				          		showType:"slide",
-				          		showSpeed:600,
-				          		title:"提示",
-				          		msg:"修改失败",
-				          		timeout:3000   		
-				          	}); 
-		            	 
-		             }
-			          	       	
-			    }) 
-			    	 $("#mytable").datagrid("reload");
-		    	 
-		     }
-		     
-		     
 
-	           },
-	
-		
+	           
+
 		 // 加载数据
 		   idField:'MUID',// 標識id字段
 			url:"cs!LoadMenu.action?v="+Math.random(), // 从哪里加载数据
@@ -275,9 +289,30 @@ function ts_menu(){
 			     
 			     },		
 			
-			     {title:'图片',field:'image',width:200,heigth:200,align:'center',
+			     {title:'图片',field:'image',width:200,heigth:200,align:'center',		    	 
+			         formatter:function(value,row,index){
+			    	 return '<img src="'+row.MUPIC+'" style="height:60px"/>';
+			    	 },
+			    	 editors:{   
+
+			    		       text: {   
+
+			    		           init: function(container, options){   
+
+			    		               var input = $('<input type="text" class="datagrid-editable-input">').appendTo(container);   
+
+			    		              return input;   
+
+			    		                                                }
+			    		           
+			               	
 			    	 
-			         formatter:function(value,row,index){return '<img src="'+row.MUPIC+'" style="height:60px"/>';}
+			                	      }
+			    	          }
+
+
+			     
+			     
 			                  
 			     }
 			     
@@ -331,7 +366,15 @@ function ts_order(){
 	})
 	$("#searchDiv").panel("expand");// 默认展开
 	$("#myTable").datagrid({
-		
+		toolbar:[
+		         {text:"导出Excl数据",iconCls:"icon-undo",handler:function(){
+		        	 
+			        	// $.post("cs!ExportMenu.action?v="+Math.random(),null,function(){})
+			        	 window.location="cs!ExportTable.action?v="+Math.random();
+			         }},
+		         
+		         
+		         ],
 	
 		
 		 // 加载数据
@@ -361,11 +404,11 @@ function ts_order(){
 			    	 }else if(value==2){
 			    		 return "<span style='color:red'><b>用户已付款</b></span>";  
 				    	 }else if(value==3){
-				    		 return "<span style='color:red'><b>已接单</b></span>";		    
-				    		 }else if(value==4){
-		    	               	 return "<span style='color:red'><b>配送中</b></span>";
-				    	          }else if(value==5){
-			    		 return "<span style='color:red'><b>交易成功</b></span>";
+				    		 return "<span style='color:#FF8C00'><b>已接单</b></span>";		    
+			    		 }else if(value==4){
+	    	               	 return "<span style='color:#FFFFFF'><b>配送中</b></span>";
+			    	          }else if(value==5){
+		    		 return "<span style='color:#999999'><b>交易成功</b></span>";
 			    	 }
                      
 		     }
@@ -443,13 +486,207 @@ function  dataview(){
 		});
 	});
 	
-	
-	
-	
-	
-	
 
 }
+
+/**
+ * 已处理订单
+ * @return
+ */
+function ts_order_yes(){
+	$("#myTable").html("");
+	
+	 $('div.datagrid-toolbar a').hide();  //隐藏
+	
+	
+	
+	$('#searchDiv').panel('close');  // 打开面板
+
+	$("#myTable").datagrid({
+		
+		      toolbar:[
+		         {text:"开始接单",iconCls:"icon-add",handler:function(){  
+		        	 setTimeout($("#myTable").datagrid("reload"),1000); //指定1秒刷新一次 
+			         }},
+		         
+		         
+		         ],
+	
+		
+		 // 加载数据
+		   idField:'OID',// 標識id字段
+			url:"cs!LoadOrderYes.action?v="+Math.random(), // 从哪里加载数据
+			fitColumns:true,// 适应宽度，防止 出现左右滚动条
+			loadMsg:"正在加载，请稍等....",
+			method:"post",
+		
+			striped:true,// 奇偶行变行
+			pagination:true,// 显示分页
+			pageList:[1,2,3,5,10,20,30,50],
+			pageSize:5,// 默认显示5条
+			rownumbers:true,// 显示行号
+			singleSelect:false,// 只允许中一行
+			columns:[[	    
+			      {field:'OID',title:'编号',width:300,align:'center',checkbox:true},
+			      {field:'USERNAME',title:'客户名称',width:300,align:'center',sortable:true},
+			     {field:'MUNAME',title:'菜单名称',width:300,align:'center',sortable:true},
+			     {field:'MUPRICE',title:'菜单单价',width:300,align:'center',sortable:true},	
+			     {field:'ODATE',title:'订单时间',width:300,align:'center',sortable:true},		
+			    {field:'OSTATUS',title:'订单状态',width:300,align:'center',sortable:true,formatter:function(value,row,index){
+		    	    if(value==0){ 
+		    		 return "<span style='color:green'><b>购物车中</b></span>";
+			    	 }else if(value==1){
+			    		 return "<span style='color:red'><b>已下单(未支付)</b></span>";
+			    	 }else if(value==2){
+			    		 return "<span style='color:red'><b>用户已付款</b></span>";  
+				    	 }else if(value==3){
+				    		 return "<span style='color:#FF8C00'><b>已接单</b></span>";		    
+				    		 }else if(value==4){
+		    	               	 return "<span style='color:#FFFFFF'><b>配送中</b></span>";
+				    	          }else if(value==5){
+			    		 return "<span style='color:#999999'><b>交易成功</b></span>";
+			    	 }
+                    
+		     }
+		     
+		     },		
+			     {field:'OCOUNT',title:'菜单数量',width:300,align:'center',sortable:true},
+			     {field:'SUM',title:'金额',width:300,align:'center',sortable:true},
+			     {field:'opt',title:'操作',width:160,align:'center',  
+			            formatter:function(value,rec){  
+			                var btn = '<a class="editcls" onclick="editRow(\''+rec.OID+'\')" href="javascript:void(0)">查看详情</a>';  
+			                return btn;  
+			            }  
+			        }  
+			    
+			]]	,	
+			 onLoadSuccess:function(data){  
+		        $('.editcls').linkbutton({text:'查看详情',plain:true,iconCls:'icon-add'});  
+		    }  
+	
+	       
+		
+
+	});
+	
+	
+	
+}
+
+/**
+ * 未处理订单
+ */
+function ts_order_no(){
+	$("#myTable").html("");
+	
+	 $('div.datagrid-toolbar a').hide();  //隐藏
+	
+	
+	
+	$('#searchDiv').panel('close');  // 打开面板
+
+	$("#myTable").datagrid({
+		
+		toolbar:[
+		         {text:"开始接单",iconCls:"icon-add",handler:function(){
+		        	 setTimeout($("#myTable").datagrid("reload"),1000); //指定1秒刷新一次 
+			         }},
+		         
+		         
+		         ],
+	
+		
+		 // 加载数据
+		   idField:'OID',// 標識id字段
+			url:"cs!LoadOrderNo.action?v="+Math.random(), // 从哪里加载数据
+			fitColumns:true,// 适应宽度，防止 出现左右滚动条
+			loadMsg:"正在加载，请稍等....",
+			method:"post",
+		
+			striped:true,// 奇偶行变行
+			pagination:true,// 显示分页
+			pageList:[1,2,3,5,10,20,30,50],
+			pageSize:5,// 默认显示5条
+			rownumbers:true,// 显示行号
+			singleSelect:false,// 只允许中一行
+			columns:[[	    
+			      {field:'OID',title:'编号',width:300,align:'center',checkbox:true},
+			      {field:'USERNAME',title:'客户名称',width:300,align:'center',sortable:true},
+			     {field:'MUNAME',title:'菜单名称',width:300,align:'center',sortable:true},
+			     {field:'MUPRICE',title:'菜单单价',width:300,align:'center',sortable:true},	
+			     {field:'ODATE',title:'订单时间',width:300,align:'center',sortable:true},		
+			    {field:'OSTATUS',title:'订单状态',width:300,align:'center',sortable:true,formatter:function(value,row,index){
+		    	    if(value==0){ 
+		    		 return "<span style='color:green'><b>购物车中</b></span>";
+			    	 }else if(value==1){
+			    		 return "<span style='color:red'><b>已下单(未支付)</b></span>";
+			    	 }else if(value==2){
+			    		 return "<span style='color:red'><b>用户已付款</b></span>";  
+				    	 }else if(value==3){
+				    		 return "<span style='color:#FF8C00'><b>已接单</b></span>";		    
+				    		 }else if(value==4){
+		    	               	 return "<span style='color:#FFFFFF'><b>配送中</b></span>";
+				    	          }else if(value==5){
+			    		 return "<span style='color:#999999'><b>交易成功</b></span>";
+			    	 }
+                     
+		     }
+		     
+		     },		
+			     {field:'OCOUNT',title:'菜单数量',width:300,align:'center',sortable:true},
+			     {field:'SUM',title:'金额',width:300,align:'center',sortable:true},
+			     {field:'opt',title:'操作',width:160,align:'center',  
+			            formatter:function(value,rec){  
+			                var btn = '<a class="editcls" onclick="editRow(\''+rec.OID+'\')" href="javascript:void(0)">接单</a>';  
+			                return btn;  
+			            }  
+			        }  
+			    
+			]]	,	
+			 onLoadSuccess:function(data){  
+		        $('.editcls').linkbutton({text:'接单',plain:true,iconCls:'icon-add'});  
+		    }  
+	
+	       
+		
+
+	});
+	
+	
+}
+/**
+ * 单机接单时
+ * @param id
+ * @return
+ */
+function editRow(id){
+	  $.post("cs!JieDanOrder.action?v="+Math.random(),{"id":id},function(data){
+		  var o = eval("("+data+")");
+		  if(o.jiedan==1){
+			  $("#myTable").datagrid("reload");  // 重新加载数据，但是数据会停留在当前页
+			  $.messager.show({
+	          		showType:"slide",
+	          		showSpeed:600,
+	          		title:"提示",
+	          		msg:"接单成功，已通知配送人员！",
+	          		timeout:3000   		
+	          	});        	
+			     
+		      }else{
+		    	  $.messager.show({
+		          		showType:"slide",
+		          		showSpeed:600,
+		          		title:"提示",
+		          		msg:"接单失败",
+		          		timeout:3000   		
+		          	});        	  
+		    	  
+		      }
+		  
+		  
+		  })
+}
+
 
 
 
